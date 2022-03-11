cfeda6b7ef68b685b34522b6f5940d5aaba245a3
Rose Lee <roselee3@uw.edu>
2022-03-10 19:02:09 -0800

    added anode, dummy 3d plot

--- GUI/selec_gui.py
+++ GUI/selec_gui.py
@@ -2,6 +2,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 import time
 
@@ -11,15 +15,26 @@ import time
 st.sidebar.image('SELEC Logo.png')
 
 
+# Currently, this code wil execute as soon as you select something
+# I'd like to change it to where it only executes when you press "calculate"
+
+# Anode Selector -----------------------------------------------
+an_df = pd.DataFrame({
+    'anodes': ['Graphite']
+    })
+
+an_option = st.sidebar.selectbox(
+    'Select an anode.',
+     an_df['anodes'])
+
 # Cathode Selector -----------------------------------------------
-cath_select = pd.DataFrame({
+cath_df = pd.DataFrame({
     'cathodes': ['NMC', 'NCA', 'LFP']
     })
 
 cath_option = st.sidebar.selectbox(
     'Select a cathode.',
-     cath_select['cathodes'])
-
+     cath_df['cathodes'])
 
 # Temp Selector ------------------------------------------------
 # st.sidebar.text_input('Temperature: ')
@@ -44,6 +59,9 @@ c_rate_option = st.sidebar.selectbox(
     'C-rate',
      c_rate_select['c-rate'])
 
+#st.write(st.session_state)
+
+
 # Cycle Selector ------------------------------------------------
 # st.sidebar.text_input('Cycle Number: ')
 cycle_select = pd.DataFrame({
@@ -60,32 +78,55 @@ st.sidebar.button('Calculate')
 
 
 # Output array of inputs to machine learning ----------------------
-front_to_back = [cath_option, temp_option, c_rate_option, cycle_option]
+front_to_back = [an_option, cath_option, cycle_option, temp_option, c_rate_option]
 
-'You selected: ', cath_option,', ', str(temp_option), 'Celsius, ', \
+'You selected: ', an_option, ', ', cath_option,', ', str(temp_option), 'Celsius, ', \
     str(c_rate_option), 'C, and cycle number ', str(cycle_option) 
 
 
 # Progress Bar ----------------------------------------------------
-'Starting a long computation...'
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
+# 'Starting a long computation...'
+# # Add a placeholder
+# latest_iteration = st.empty()
+# bar = st.progress(0)
 
-n = 100
-for i in range(n):
-  # Update the progress bar with each iteration.
-  bar.progress(i + 1)
-  time.sleep(0.1)
+# # we have to update this to reflect the actual computational time
+# n = 100
+# for i in range(n):
+#   # Update the progress bar with each iteration.
+#   bar.progress(i + 1)
+#   time.sleep(0.1)
 
-'...and now we\'re done!'
+# '...and now we\'re done!'
 
 
 # Data Visualization -----------------------------------------------
+sns.set_context('talk')
 
-'Open Circuit Voltage'
-OCV = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+x = [1,1,1] # input, strings (ex: 'NMC')
+y = [50,100,150] # input
+z = [13, 69, 21] # output
 
-st.line_chart(OCV)
+
+fig = plt.figure(figsize = (5,5))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(x, y, z, '--o', markersize = 10, label = 'line :)')
+ax.set_title('meaningless chart')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.legend()
+
+fig
+
+# This was my Demo
+# 'Open Circuit Voltage'
+# OCV = pd.DataFrame(
+#      np.random.randn(20, 3),
+#      columns=['NMC', 'NCA', 'LFP'])
+
+# st.line_chart(OCV)
+
+# data frames --> output, MSE
+# we're interested in a 3D plot
+# axes: battery system, cycle, output, 
